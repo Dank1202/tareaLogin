ab76d463fd5717acd22f39b46ad90b31b0470654
Jaime Esqueda <jaimeesqueda@MacBook-Air-de-Jaime.local>
2025-02-02 22:16:21 -0600

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { PrincipalComponent } from './principal/principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'principal', component: PrincipalComponent },
+];
